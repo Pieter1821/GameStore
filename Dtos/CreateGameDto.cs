1c40bf577c7e988fa70903edeae4633e0696d397
Pieter Deane <deanepieter@gmail.com>
2024-11-06 16:04:59 +0200

    fixed POST method error  added double quotes for key value pair

--- Dtos/CreateGameDto.cs
+++ Dtos/CreateGameDto.cs
@@ -2,9 +2,8 @@
 
 public class CreateGameDto
 {
-    public required string Title { get; set; }
-    public required  string Genre { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
-
 }
