ab4deb56640d7a59547cb034880dbfb8bc844d1f
Pieter Deane <deanepieter@gmail.com>
2024-11-11 06:40:16 +0200

    adding connection string  , adding migrations safely ,load env variables

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using GameStore.API.Data;
 using GameStore.API.Dtos;
 using GameStore.API.Endpoints;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Load environment variables from .env file
+Env.Load();
+
+var connString = Environment.GetEnvironmentVariable("GAME_STORE_DB");
+if (string.IsNullOrEmpty(connString))
+{
+    throw new InvalidOperationException("Connection string not found.");
+}
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
