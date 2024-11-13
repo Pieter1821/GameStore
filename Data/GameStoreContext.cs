045bf0942650f670a2d0b85deaf0955e510a36e8
Pieter Deane <deanepieter@gmail.com>
2024-11-13 07:11:35 +0200

    added migrations to seed the data of genres

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -4,12 +4,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.API.Data
 {
-    public class GameStoreContext(DbContextOptions<GameStoreContext>options)
-        :DbContext(options)
+    public class GameStoreContext : DbContext
     {
-        public DbSet<Game> Games => Set<Game>();
+        public GameStoreContext(DbContextOptions<GameStoreContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<Game> Games => Set<Game>();
         public DbSet<Genre> Genres => Set<Genre>();
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre { Id = 1, Name = "Platformer" },
+                new Genre { Id = 2, Name = "Action-adventure" },
+                new Genre { Id = 3, Name = "Sandbox" },
+                new Genre { Id = 4, Name = "Party" },
+                new Genre { Id = 5, Name = "First-person shooter" }
+            );
+        }
     }
 }
